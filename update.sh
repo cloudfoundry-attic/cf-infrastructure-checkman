4e3b808557323c17b3787f26d0ea47e5ace38600
Ryan Moran <rmoran@pivotal.io>
2016-03-17 11:38:19 -0700

    Adds update script for mega-ci checklists

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -exu
+
+function main() {
+  local pipelines
+  pipelines=("bootload-mega-ci" "bosh-bootloader" "bosh-test" "consul" "destiny" "etcd" "gomegamatchers" "mega-ci")
+
+  for pipeline in ${pipelines[@]}; do
+    fly -t mega checklist -p "${pipeline}" > "${pipeline}"
+  done
+}
+
+main
