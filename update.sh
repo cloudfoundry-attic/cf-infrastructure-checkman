3005f92ff55b4d733aaba12879058c2a82081be7
Amit Kumar Gupta <agupta@pivotal.io>
2016-07-26 17:32:44 -0700

    Add check-a-record

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 function main() {
   local pipelines
-  pipelines=("bootload-mega-ci" "bosh-bootloader" "bosh-test" "consul" "destiny" "etcd" "gomegamatchers" "mega-ci")
+  pipelines=("bootload-mega-ci" "bosh-bootloader" "bosh-test" "consul" "check-a-record" "destiny" "etcd" "gomegamatchers" "mega-ci")
 
   for pipeline in ${pipelines[@]}; do
     fly -t mega checklist -p "${pipeline}" > "${pipeline}"
